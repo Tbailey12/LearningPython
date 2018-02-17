80062d785eba770ef26b19e9eb1aafa376de4995
Tom_Desktop <tom.bailey13@outlook.com>
2018-02-17 13:51:20 +1100

    space invaders initialized

---
+++ SpaceInvaders/space_invaders.py
@@ -0,0 +1,10 @@
+"""
+    File name:          space_invaders.py
+    Author:             Tom Bailey
+    Date created:       2018-02-17
+    Date last modified: 2018-02-17
+    Python Version:     3.6
+
+    Description:
+    basic space invaders game to practice rendering
+"""
