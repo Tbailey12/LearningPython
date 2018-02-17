f14451f73483913bf4b9ffcf46ccb77ed46afdef
Tom_Desktop <tom.bailey13@outlook.com>
2018-02-17 14:04:42 +1100

    Created window and player turtle

--- SpaceInvaders/space_invaders.py
+++ SpaceInvaders/space_invaders.py
@@ -1,5 +1,6 @@
 """
     File name:          space_invaders.py
+    Version:            0.0
     Author:             Tom Bailey
     Date created:       2018-02-17
     Date last modified: 2018-02-17
@@ -9,4 +10,34 @@
     basic space invaders game to practice rendering
 """
 
-print('space invaders test')
+import turtle
+import os
+
+# screen setup
+win = turtle.Screen()
+win.bgcolor('black')
+win.title('Space Invaders')
+
+# draw game border
+border_pen = turtle.Turtle()
+border_pen.speed(0)  # 0 is fastest speed
+border_pen.pensize(3)
+border_pen.color('white')
+border_pen.penup()  # pen is not touching canvas
+border_pen.setposition(-300, -300)
+border_pen.pendown()
+for side in range(4):
+    border_pen.fd(600)
+    border_pen.left(90)
+border_pen.hideturtle()
+
+# create the player turtle
+player = turtle.Turtle()
+player.color('blue')
+player.shape('triangle')
+player.penup()
+player.speed(0)
+player.setposition(0, -250)
+player.setheading(90)
+
+delay = input('Press enter to close')
