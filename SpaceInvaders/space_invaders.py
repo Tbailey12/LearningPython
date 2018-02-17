5d81e051879ca61005879888c7b30b2d38e4f424
Tom_Desktop <tom.bailey13@outlook.com>
2018-02-17 14:39:53 +1100

    shoot bullet and reload. - a bit laggy Tutorial 5 https://www.youtube.com/watch?v=OnR9acEWMC0

--- SpaceInvaders/space_invaders.py
+++ SpaceInvaders/space_invaders.py
@@ -42,25 +42,102 @@ player.setheading(90)
 
 player_speed = 15  # controls the movement speed of the player
 
+# create the enemy turtle
+enemy = turtle.Turtle()
+enemy.color('red')
+enemy.shape('circle')
+enemy.penup()
+enemy.speed(0)
+enemy.setposition(-200, 250)
 
-# move the player left and right
+enemy_speed = 2
+
+# create player bullet
+bullet = turtle.Turtle()
+bullet.color('yellow')
+bullet.shape('triangle')
+bullet.penup()
+bullet.speed(0)
+bullet.setheading(90)
+bullet.shapesize(0.5, 0.5)
+bullet.hideturtle()
+
+bullet_speed = 20
+
+# define bullet state
+# ready - ready to fire
+# fire - bullet is fired
+bullet_state = 'ready'
+
+
 def move_left():
+    """
+    move player left
+    :return: none
+    """
     x = player.xcor()  # gets player xcoord
     x -= player_speed
+    if x < -280:  # boundary checking
+        x = -280
     player.setx(x)
 
 
 def move_right():
+    """
+    move player right
+    :return: none
+    """
     x = player.xcor()  # gets player xcoord
     x += player_speed
+    if x > 280:  # boundary checking
+        x = 280
     player.setx(x)
+
+
+def player_shoot():
+    global bullet_state
+
+    if bullet_state == 'ready':
+        bullet_state = 'fire'
+        # move the bullet to player
+        x = player.xcor()
+        y = player.ycor() + 10
+        bullet.setposition(x, y)
+        bullet.showturtle()
 
 
 # create keyboard bindings
 turtle.listen()
 turtle.onkey(move_left, "Left")
 turtle.onkey(move_right, "Right")
+turtle.onkey(player_shoot, "space ")
 
-turtle.done()
+# main game loop
+while True:
+    # move the enemy
+    x = enemy.xcor()
+    x += enemy_speed
+    enemy.setx(x)
 
-delay = input('Press enter to close')
+    if enemy.xcor() > 280:
+        enemy_speed *= -1  # change direction at boundary
+        y = enemy.ycor()
+        y -= 40  # drop down a level at boundary
+        enemy.sety(y)
+    elif enemy.xcor() < -280:
+        enemy_speed *= -1
+        y = enemy.ycor()
+        y -= 40
+        enemy.sety(y)
+
+    # move the bullet if it is on the screen
+    if bullet_state == 'fire':
+        y = bullet.ycor()
+        y += bullet_speed
+        bullet.sety(y)
+        if bullet.ycor() > 280:
+            bullet.hideturtle()
+            bullet_state = 'ready'
+
+# turtle.done()
+# delay = input('Press enter to close')
